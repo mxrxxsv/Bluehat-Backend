29aada29faff46f53158a295b518d2046097e4f0
mxrxxsv <130885953+mxrxxsv@users.noreply.github.com>
2025-04-19 22:41:01 +0800

    Update WorkerPortfolio.jsx

--- frontend/src/Components/WorkerPortfolio.jsx
+++ frontend/src/Components/WorkerPortfolio.jsx
@@ -28,21 +28,21 @@ const WorkerPortfolio = () => {
           // src={worker.profileImage || "/default-profile.png"}
           src={profile}
           alt={worker.name}
-          className="w-32 h-32 object-cover rounded-md border"
+          className="w-32 h-32 object-cover rounded-full border"
         />
         <div className="flex-1 space-y-1 text-left">
           <h1 className="text-3xl font-bold">{worker.name}</h1>
           <p className="text-gray-700">{worker.description}</p>
-          <p className="text-sm text-gray-500">📍 {worker.location}</p>
+          <p className="text-sm text-gray-500">{worker.location}</p>
           <p className="text-sm text-gray-500">
-            🎂 Age: {worker.age} • 🧍 Gender: {worker.gender}
+            Age: {worker.age} •  Gender: {worker.gender}
           </p>
 
           <div className="mt-3 flex gap-2">
-            <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+            <button className="p-2 bg-blue-500 text-white shadow-md rounded-[14px] hover:bg-blue-600 hover:shadow-lg cursor-pointer">
               Message
             </button>
-            <button className="px-4 py-2 bg-gray-200 text-gray-700 rounded hover:bg-gray-300">
+            <button className="px-4 py-2 bg-gray-200 text-gray-700 shadow-md rounded-[14px] hover:bg-gray-300 hover:shadow-lg cursor-pointer">
               Save
             </button>
           </div>
@@ -56,7 +56,7 @@ const WorkerPortfolio = () => {
           {worker.skills.map((skill, index) => (
             <span
               key={index}
-              className="px-3 py-1 bg-blue-100 text-black-800 text-sm rounded-full"
+              className="px-3 py-1 bg-[#55b3f3] shadow-sm text-[#f4f6f6] text-[14px] rounded-full"
             >
               {skill}
             </span>
@@ -71,7 +71,7 @@ const WorkerPortfolio = () => {
         </h2>
         <div className="space-y-4">
           {worker.experience.map((exp, index) => (
-            <div key={index} className="border p-4 rounded-md text-left">
+            <div key={index} className="shadow p-4 my-2 rounded-md text-left bg-white shadow-sm">
               <h3 className="font-semibold text-lg">{exp.company}</h3>
               <p className="text-sm text-gray-500">
                 {exp.years} • {exp.position}
@@ -91,11 +91,11 @@ const WorkerPortfolio = () => {
           </span>
         </div>
 
-        <div className="space-y-4">
+        <div className="space-y-2">
           {worker.reviews.map((review, index) => (
             <div
               key={index}
-              className="flex items-start gap-4 border p-4 rounded-md text-left"
+              className="flex items-start gap-4 p-4 rounded-md text-left bg-white shadow-sm"
             >
               <img
                 // src={review.clientImage || "/default-client.png"}
