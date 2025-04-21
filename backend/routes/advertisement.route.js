5ad246fe0e31b90a025ce1b1b9496a359b44bb63
RichmondCg <RichmondGillaco.neust@gmail.com>
2025-04-21 23:52:33 +0800

    get advertisement route

--- backend/routes/advertisement.route.js
+++ backend/routes/advertisement.route.js
@@ -2,20 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const Advertisement = require("../models/Advertisement");
+const { getAdvertisement } = require("../controllers/worker.controller");
 
-router.get("/", async (req, res) => {
-  try {
-    const advertisement = await Advertisement.find({});
-    if (advertisement.length === 0) {
-      res
-        .status(404)
-        .json({ success: false, message: "There are no advertisement" });
-    }
-    res.status(200).json({ success: true, data: advertisement });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
-});
+router.get("/", getAdvertisement);
 
 router.post("/add-ad", async (req, res) => {
   const { companyName, adTitle, description, category } = req.body;
