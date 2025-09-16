ec6d70959fd4f095506a647478a0bf3df3253c9f
mxrxxsv <130885953+mxrxxsv@users.noreply.github.com>
2025-09-16 22:13:31 +0800

    Changing name of Route and Controller

--- backend/routes/userIDVerification.route.js
+++ backend/routes/userIDVerification.route.js
@@ -9,7 +9,7 @@ const {
   getVerificationStatus,
   deleteDocument,
   getPendingVerifications,
-} = require("../controllers/userIDVerification");
+} = require("../controllers/userIDVerification.controller");
 const verifyToken = require("../middleware/verifyToken");
 const verifyAdmin = require("../middleware/verifyAdmin");
 const router = express.Router();
