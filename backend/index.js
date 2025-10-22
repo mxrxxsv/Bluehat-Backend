00d7dacd9ee47bd3a65cb314469720ee7f9a9d4d
mxrxxsv <mariusjamess26@gmail.com>
2025-10-22 19:53:26 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ const contractRoute = require("./routes/workContract.route");
 
 const allowedOrigins = [process.env.FRONTEND_URL];
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 // 1) Security headers
 app.use(
