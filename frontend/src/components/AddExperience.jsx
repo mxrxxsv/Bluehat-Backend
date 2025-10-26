0763e6ebe5d30f81f528c8b264d8b606420b5d0b
mxrxxsv <mariusjamess26@gmail.com>
2025-10-26 22:30:39 +0800

    Updating UI

--- frontend/src/components/AddExperience.jsx
+++ frontend/src/components/AddExperience.jsx
@@ -47,7 +47,7 @@ const AddExperience = ({ onClose, onAdd, onRefresh }) => {
   return (
     <>
       {/* Modal */}
-      <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+      <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4">
         <div className="bg-white rounded-2xl p-6 w-[28rem] relative shadow-lg">
           {/* Close button */}
           <button
